95511322a28286c60ec633d37ab4ddc8c1493065
bighole-69 <09bighole@gmail.com>
2025-11-04 13:59:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('웹 만들기')
 name=st.text_input('이름')
-st.selectbox('좋아하는 음식은 뭔가',['고기','채소']
+st.selectbox('좋아하는 음식은 뭔가'),['고기','채소']
 if st.button('인사말'):
   st.write(name+'안녕하신가')
