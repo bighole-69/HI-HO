0c2d838d6012506533f57dd7ef62828d3c8d0add
bighole-69 <09bighole@gmail.com>
2025-11-04 14:07:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ st.title('웹 만들기')
 name=st.text_input('이름')
 menu=st.selectbox('좋아하는 음식은 뭔가요',['고기','채소'])
 if st.button('인사말'):
-  st.info(name+'안녕하신가')
+  st.info(name+'안녕하신가요')
   st.warning(menu+'를 좋아하나보군요, 저도 좋아합니다')
+  st.error('반갑습니다')
